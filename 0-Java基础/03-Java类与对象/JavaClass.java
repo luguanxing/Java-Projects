c37d958b83d41fc6c6fbdd04caf1c65b716e1a7f
luguanxing <651571925@qq.com>
2017-02-17 23:00:41 +0800

    JavaClass

---
+++ 0-Java基础/03-Java类与对象/JavaClass.java
@@ -0,0 +1,60 @@
+class Person {
+	private String name;	//私有成员
+	private int age;
+	
+	public Person() {	//构造函数和析构函数
+		name = "undefined";
+		age = 0;
+	}
+	public Person(String name) {
+		this();
+		this.name = name;
+	}
+	public Person(int age) {
+		this();
+		this.age = age;
+	}
+	public Person(String name, int age) {
+		this();
+		this.name = name;
+		this.age = age;
+	}
+	public void finalize() {
+		System.out.println("finalize.");
+	}
+	
+	public String GetName() {	//方法
+		return this.name;
+	}
+	public int GetAge() {
+		return this.age;
+	}
+	public void speak() {
+		System.out.println("My name is "+this.name+", I am "+this.age+" years old.");
+	}
+}
+
+
+public class JavaClass {
+	public static void main(String[] args) {
+		Person person0 = new Person();
+		Person person1 = new Person("123");
+		Person person2 = new Person(321);
+		Person person3 = new Person("123", 321);
+			
+		person0.speak();
+		person1.speak();
+		person2.speak();
+		person3.speak();
+		
+		person0 = null;
+		person1 = null;
+		person2 = null;
+		person3 = null;
+		
+		System.gc();
+		for (int i = 0; i < 100000; i++) {
+			//wait for finalizing
+		}
+	}
+}
