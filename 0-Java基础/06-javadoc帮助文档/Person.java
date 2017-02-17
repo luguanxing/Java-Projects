2cf4caf6a2a3c9e1602f9a89a809b58290eded4c
luguanxing <651571925@qq.com>
2017-02-18 00:54:00 +0800

    javadoc

---
+++ 0-Java基础/06-javadoc帮助文档/Person.java
@@ -0,0 +1,27 @@
+/**
+ * <b>Title:</b><br>javadoc测试<br><br>
+ * <b>Description:</b><br>用javadoc生成文档来进行测试<br><br>
+ * @author luguanxing
+ * @version: 1.0
+ */
+public class Person {
+	public String name;
+	
+	
+	/**
+	 * 这是Person类的构造方法
+	 * @param name Person的名字
+	 */
+	public Person(String name) {
+		//构造函数
+	}
+	
+	/**
+	 * 这是Speak方法的说明
+	 * @param message 说话内容
+	 */
+	public void Speak(String message) {
+		System.out.println(message);
+	}
+	
+}
