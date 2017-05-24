a77e57764b720aa9ed43f44d90ea4ee8534ff625
luguanxing <651571925@qq.com>
2017-05-24 09:13:52 +0800

    Java基础

--- 00-Java基础/16-JavaFile/TestFile.java
+++ 00-Java基础/16-JavaFile/TestFile.java
@@ -10,19 +10,19 @@ public class TestFile {
 	}
 	
 	public static void testFile() {
-        File file=new File("十不准.txt");  
-        System.out.println("判断文件是否存在"+file.exists());  
-        System.out.println("读取文件名称"+file.getName()); 
-        System.out.println("读取文件修改日期"+new SimpleDateFormat("yyyy-MM-dd HH:mm:ss").format(new Date(file.lastModified()))); 
-        System.out.println("读取文件相对路径"+file.getPath());  
-        System.out.println("读取文件绝对路径"+file.getAbsolutePath());  
-        System.out.println("读取文件的父级路径"+new File(file.getAbsolutePath()).getParent());  
-        System.out.println("读取文件的大小"+file.length()+"byte");
-        System.out.println("判断文件是否被隐藏"+file.isHidden());  
-        System.out.println("判断文件是否可读"+file.canRead());  
-        System.out.println("判断文件是否可写"+file.canWrite());  
-        System.out.println("判断文件是否为文件夹"+file.isDirectory());
-        System.out.println("");
+		File file=new File("十不准.txt");  
+		System.out.println("判断文件是否存在"+file.exists());  
+		System.out.println("读取文件名称"+file.getName()); 
+		System.out.println("读取文件修改日期"+new SimpleDateFormat("yyyy-MM-dd HH:mm:ss").format(new Date(file.lastModified()))); 
+		System.out.println("读取文件相对路径"+file.getPath());  
+		System.out.println("读取文件绝对路径"+file.getAbsolutePath());  
+		System.out.println("读取文件的父级路径"+new File(file.getAbsolutePath()).getParent());  
+		System.out.println("读取文件的大小"+file.length()+"byte");
+		System.out.println("判断文件是否被隐藏"+file.isHidden());  
+		System.out.println("判断文件是否可读"+file.canRead());  
+		System.out.println("判断文件是否可写"+file.canWrite());  
+		System.out.println("判断文件是否为文件夹"+file.isDirectory());
+		System.out.println("");
 	}
 	
 	public static void SearchDir() {
