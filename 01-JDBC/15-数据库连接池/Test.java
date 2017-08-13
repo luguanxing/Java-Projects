1cd41bde2c93c826fe29d6e659cd04cca0f11705
luguanxing <651571925@qq.com>
2017-08-14 00:44:53 +0800

    数据库连接池

---
+++ 01-JDBC/15-数据库连接池/Test.java
@@ -0,0 +1,34 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.TreeSet;
+
+import com.mysql.jdbc.Statement;
+
+import jdbc.JdbcUtils;
+
+public class Test {
+
+	public static void main(String[] args) throws Exception {
+		多次连接测试();
+		超载测试();
+	}
+	
+	public static void 多次连接测试() throws Exception {
+		System.out.println("多次连接测试");
+		for (int i = 0; i < 10; i++) {
+			Connection conn = JdbcUtils.getConnection();
+			System.out.println(conn);
+			JdbcUtils.free(null, null, conn);
+		}
+	}
+	
+	public static void 超载测试() throws Exception {
+		System.out.println("超载测试");
+		for (int i = 0; i < 10; i++) {
+			Connection conn = JdbcUtils.getConnection();
+			System.out.println(conn);
+		}
+	}
+}
